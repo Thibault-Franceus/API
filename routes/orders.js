18a8024cf2314bfcd221ae1ad3760d3ce454a79b
Thibault-Franceus <r1006946@student.thomasmore.be>
2024-12-08 15:40:18 +0100

    feat: require userid

--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const ordersController = require('../controllers/orders');
+const passport = require('passport');
 
 router.get('/', ordersController.getOrders);
 router.post('/', ordersController.createOrder);
+router.post('/', passport.authenticate('jwt', {session:false}), ordersController.createOrder);
+router.get('/:id', passport.authenticate('jwt', {session:false}), ordersController.getOrder);
+
+
+
 router.put('/:id', ordersController.updateOrder);
 router.delete('/:id', ordersController.deleteOrder);
 module.exports = router;
